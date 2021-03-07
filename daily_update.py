035dba38e79a86cb13f86f2f3fb7b17c4bcfc7a0
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-03-06 23:54:24 -0800

    update_url gathers rss trials to update

--- daily_update.py
+++ daily_update.py
@@ -1,9 +1,8 @@
 import feedparser as fp
 import requests
 
-
 def rss_feed():
-    print('Gatering updated NCT trial numbers from clinicaltrials.gov...')
+    print('Gatering updated NCT trial numbers from clinicaltrials.gov...\n')
     nctids = []
     
     url = 'https://clinicaltrials.gov/ct2/results/rss.xml?rcv_d=&lup_d=2&sel_rss=mod2&recrs=a&type=Intr&cond=Cancer&cntry=US&count=10000'
@@ -13,13 +12,26 @@ def rss_feed():
         nctid = item.id
         nctids.append(nctid)
     return nctids
-
-def update():
+        
+def update_url():
     studies = rss_feed()
+    print(f'Found {len(studies)} studies to update.\n')
 
     for ID in studies:
-        full_study = f'https://clinicaltrials.gov/api/query/full_studies?expr={ID}&min_rnk=1&max_rnk=&fmt=json'
-        print(full_study)
+        base_url = 'https://clinicaltrials.gov/api/query/study_fields?'
+        expr = f'expr={ID}&' # change expression to search for disease, device, etc.
+        field = 'fields=StudyType%2C+NCTId%2COfficialTitle%2C+StartDate%2C+PrimaryCompletionDate%2C+LastUpdatePostDate%2C+Condition%2C+Gender%2C+MaximumAge%2C+EligibilityCriteria%2C+CentralContactName%2C+CentralContactPhone%2C+CentralContactEMail%2C+LocationFacility%2C+LocationCity%2C+LocationState%2C+LocationZip%2C+LeadSponsorName&'
+        fmt_json = 'min_rnk=1&max_rnk=1&fmt=json'
+        fmt_csv = 'min_rnk=1&max_rnk=1&fmt=csv'
         
+        #choose to return json or csv. TODO: add command line/install prompt. JSON works with MatchaMiner, FYI
+        id_json = f'{base_url}{expr}{field}{fmt_json}\n'
+        #id_csv = f'{base_url}{expr}{field}{fmt_csv}\n'
+        print(id_jsom)
+    return id_json
+
+def main():
+    update_url()
+
 if __name__ == '__main__':
-    update()
+    main()
