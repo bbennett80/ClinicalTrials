2efed82bb2b7d5248ac97edcddf4f2956286725e
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-03-20 00:01:56 -0700

    Update daily_update.py

--- daily_update.py
+++ daily_update.py
@@ -1,5 +1,11 @@
 import feedparser as fp
 import requests
+import pandas as pd
+import json
+
+def main():
+    download_trials()
+
 
 def rss_feed():
     print('Gatering updated NCT trial numbers from clinicaltrials.gov...\n')
@@ -7,33 +13,34 @@ def rss_feed():
     url = 'https://clinicaltrials.gov/ct2/results/rss.xml?rcv_d=&lup_d=2&sel_rss=mod2&recrs=a&type=Intr&cond=Cancer&cntry=US&count=10000'
     d = fp.parse(url)
     
-    nctids = []
-
-    for item in d.entries:
-        nctid = item.id
-        nctids.append(nctid)
-    return nctids
-        
-def update_url():
-    studies = rss_feed()
-    print(f'Found {len(studies)} studies to update.\n')
-
-    updated_study_urls = []
-    for ID in studies:
+    nctids = [item.id for item in d.entries]
+    print(f'Found {len(nctids)} studies to update.\n')
+    
+    rss_items = []
+    
+    for ID in nctids:
         base_url = 'https://clinicaltrials.gov/api/query/study_fields?'
         expr = f'expr={ID}&'
         field = 'fields=StudyType%2C+NCTId%2COfficialTitle%2C+StartDate%2C+PrimaryCompletionDate%2C+LastUpdatePostDate%2C+Condition%2C+Gender%2C+MaximumAge%2C+EligibilityCriteria%2C+CentralContactName%2C+CentralContactPhone%2C+CentralContactEMail%2C+LocationFacility%2C+LocationCity%2C+LocationState%2C+LocationZip%2C+LeadSponsorName&'
-        #fmt_json = 'min_rnk=1&max_rnk=1&fmt=json'
-        fmt_csv = 'min_rnk=1&max_rnk=1&fmt=csv'
+        fmt_json = 'min_rnk=1&max_rnk=1&fmt=json'
+        #fmt_csv = 'min_rnk=1&max_rnk=1&fmt=csv'
+        url = f'{base_url}{expr}{field}{fmt_json}'
+        study = ID, url
+        rss_items.append(study)
         
-        #choose to return json or csv. TODO: add command line/install prompt. JSON works with MatchaMiner, FYI
-        #id_json = f'{base_url}{expr}{field}{fmt_json}\n'
-        id_csv = f'{base_url}{expr}{field}{fmt_csv}\n'
-        updated_studies.append(id_csv)
-    return updated_study_urls
+    return rss_items
 
-def main():
-    update_url()
+
+def download_trials():
+    rss_items = rss_feed()
+    print('\nDownloading: ')
+    
+    for ID, URL in rss_items:
+        r = requests.get(URL).json()
+        raw_json = r['StudyFieldsResponse']['StudyFields'][0]
+        print(f'{ID}')
+        with open(f'{ID}.json', 'w+') as f:
+            json.dump(raw_json, f, indent=2)
 
 if __name__ == '__main__':
     main()
