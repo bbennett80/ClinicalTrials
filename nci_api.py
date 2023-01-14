28b7d0deb6ae5817bb8f464a957ce231820ace3a
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2023-01-13 16:13:50 -0800

    Update nci_api.py

--- nci_api.py
+++ nci_api.py
@@ -1,6 +1,9 @@
 import requests
 from KEYS import API_KEY
 
+NCT_ID = " "
+# NCT number/ID as string
+
 def disease_query(disease_keyword: str):
     """A query to be passed to the request function. 
     'disease_keywords' are passed to a list of strings.
@@ -37,48 +40,66 @@ def disease_query(disease_keyword: str):
     
     return query_params
 
-def get_trial_information(nct_number: str):
-    """Uses GET method to API based on 'NCT#'"""
+def get_trial_by_nctid(NCT_ID: str):
+    """Uses the trial NCT number to return JSON for study."""
     
-    api_endpoint = f"https://clinicaltrialsapi.cancer.gov/api/v2/trials/{nct_number}"
-    
-    res = requests.get(
-            api_endpoint, 
-            headers = {"Content-Type": "application/json", "x-api-key": API_KEY},
-            verify=True
-        )    
-    
+    API_ENDPOINT = f"https://clinicaltrialsapi.cancer.gov/api/v2/trials/{NCT_ID}"
+    HEADERS = {"Content-Type": "application/json", "x-api-key": API_KEY}
+
+    res = requests.get(API_ENDPOINT, 
+                       headers=HEADERS,
+                       verify=True
+                      )
+
     if res.status_code != 200:
         raise ValueError("Failed to retrieve trial information. Check trial number.")
     else:
-        return res.json(), res.url
+        return res.json()
+        
 
-
-def parse_eligibility_criteria(trial_data):
+def criteria(trial):
     """Parse trial data in to inclusion/exclusion criteria"""
+    
+    official_title = trial['official_title']
+    brief_summary = trial['brief_summary']
+    detail_description = trial['detail_description']
+    
+    structured_eligibility = trial['eligibility']['structured']
+    gender = structured_eligibility['gender']
+    min_age = int(structured_eligibility['min_age_in_years'])
+    max_age = int(structured_eligibility['max_age_in_years'])
+
+    details = [official_title, brief_summary, detail_description, gender, min_age, max_age]
+
     inclusion_criteria = []
     exclusion_criteria = []
     
-    structured_criteria = trial_data['structured']
-    inclusion_criteria.append(int(structured_criteria['min_age_number']))
-    inclusion_criteria.append(int(structured_criteria['max_age_number']))
-    inclusion_criteria.append(structured_criteria['gender'])
-    
-    
-    unstructured_criteria = trial_data['unstructured']
-    
-    for criteria in unstructured_criteria:
-        if criteria['inclusion_indicator']:
-            inclusion_criteria.append(criteria['description'])
-        else:
-            exclusion_criteria.append(criteria['description'])
 
-    return inclusion_criteria, exclusion_criteria
+    unstructured_eligibility = trial['eligibility']['unstructured']
+    
+    for item in unstructured_eligibility:
+        if item['inclusion_indicator']: inclusion_criteria.append(item['description'])
+        else: exclusion_criteria.append(item['description'])
+    
+    return details, inclusion_criteria, exclusion_criteria
 
 
-if __name__ == "__main__":
-    nct_number = "NCT04214262"
-    trial, query_url = get_trial_information(nct_number)
-    inclusion_criteria, exclusion_criteria = parse_eligibility_criteria(trial['eligibility'])
-    print(inclusion_criteria)
-    print(exclusion_criteria)
+def print_in_notebook_or_terminal(details, inclusion_criteria, exclusion_criteria):
+    print(f'NCT#: {NCT_ID}\n\n'
+          f'Official Title: {details[0]}\n\n' 
+          f'Summary: {details[1]}\n\n' 
+          f'Description: \n{details[2]}\n\n')
+    
+    print(f'Gender: {details[3]}\n\n'
+          f'Mininum Age: {details[4]}\n\n'
+          f'Maximum Age: {details[5]}\n\n')
+    
+    print('***Inclusion criteria:***')
+    for i in inclusion_criteria:
+          print(f'[ ] {i}\n')
+
+    print('***Exclusion criteria:***')
+    for i in exclusion_criteria:
+          print(f'[ ] {i}\n')
+            
+    return
